d43fecc674b3de534b02e376159dcd9080e32d55
NiyomugaboFidel <fidelniyomugabo67@gmail.com>
2024-11-02 11:22:55 +0200

    add first commit

---
+++ store/authSlice.tsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+     user:[]
+}
+
+const authSlice =  createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+
+    }
+})
+
